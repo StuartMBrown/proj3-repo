4fad5ba36be522b0ad969567eb4feb86fd7019ec
Kelsey <Kelseys2071@gmail.com>
2024-01-05 14:27:07 -0600

    adding in updates

--- static/js/index.js
+++ static/js/index.js
@@ -30,22 +30,3 @@ const speciesData = [
   console.log('Threatened Species Count:', threatenedSpeciesCount);
   console.log('Endangered Species Count:', endangeredSpeciesCount);
   console.log('Nonessential Species Count:', nonessentialSpeciesCount);
-// map.js
-const map = L.map('map').setView([37.8, -96], 4); // Set the initial view to the United States
-// Add base layer for states
-const statesLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  attribution: '© OpenStreetMap contributors'
-}).addTo(map);
-// Add your threatened species heatmap layer
-const threatenedHeatLayer = L.heatLayer(threatenedSpeciesData, { radius: 20 }).addTo(map);
-// Add your endangered species heatmap layer
-const endangeredHeatLayer = L.heatLayer(endangeredSpeciesData, { radius: 20 }).addTo(map);
-// Add your nonessential species heatmap layer
-const nonessentialHeatLayer = L.heatLayer(nonessentialSpeciesData, { radius: 20 }).addTo(map);
-// Create a control layer to toggle different species layers
-const overlayMaps = {
-  'Threatened Species': threatenedHeatLayer,
-  'Endangered Species': endangeredHeatLayer,
-  'Nonessential Species': nonessentialHeatLayer
-};
-L.control.layers({ 'States': statesLayer }, overlayMaps).addTo(map);
